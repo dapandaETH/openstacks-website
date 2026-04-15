04af01c64e480c274bac1423e7d2f64efdf550ec
P <p@Ps-MacBook-Pro.local>
2026-04-15 14:36:10 +0800

    Add Navigation component with logo and links

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,30 @@
+import { Link, useLocation } from 'react-router-dom';
+import './Navigation.css';
+
+function Navigation() {
+  const location = useLocation();
+  
+  return (
+    <nav className="navigation">
+      <div className="nav-logo">
+        <Link to="/">OpenStacks</Link>
+      </div>
+      <div className="nav-links">
+        <Link 
+          to="/" 
+          className={location.pathname === '/' ? 'active' : ''}
+        >
+          Coding Plans
+        </Link>
+        <Link 
+          to="/about" 
+          className={location.pathname === '/about' ? 'active' : ''}
+        >
+          About
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
